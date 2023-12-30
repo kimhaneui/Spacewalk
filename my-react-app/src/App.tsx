d41901c5fe742f693052404cac562294cefba630
kimhaneui <61342572+kimhaneui@users.noreply.github.com>
2023-12-30 19:22:10 +0900

    Feat: 404 add

--- my-react-app/src/App.tsx
+++ my-react-app/src/App.tsx
@@ -1,6 +1,7 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './Home';
+import NotFound from './NotFound';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Router>
         <Routes>
           <Route path="/home" element={<Home />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </Router>
     </div>
